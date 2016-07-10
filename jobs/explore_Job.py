4b92c2f7d94834960e02a4bfe2c5f8eb1974e780
andrewhan <itsandrewhan@gmail.com>
2016-07-10 01:40:25 -0400

    filestructure changes, added Driver and testDriver

---
+++ jobs/explore_Job.py
@@ -0,0 +1,75 @@
+#Lindsay_Job1 - analysis script
+
+from TrackClass import *
+from general import *
+import _pickle as pickle
+from copy import deepcopy
+import pylab as P
+import numpy as np
+from scipy import stats
+import csv
+import xlsxwriter
+from scipy import stats
+import TrackClassGlobals as TCG
+from PlotScripts import *
+
+SAVE = True
+
+ALL_EXP_FOLDER_PATH = "C:\\Users\Andrew Han\\Desktop\\track files\select fibers/"
+DATA_SAVE_NAME = 'nanofiberDiameter'
+
+#SAVE DATA
+if SAVE is True:
+	data = importAndSave(ALL_EXP_FOLDER_PATH, DATA_SAVE_NAME)
+
+####################################
+#LOAD DATA
+with open(DATA_SAVE_NAME, 'rb') as input:
+	data = pickle.load(input)
+
+
+	filters = TCG.DefaultFilters.copy()
+	filters['age'] = [[5, float('inf')]]
+
+	ps = TCG.PlotDefaults.copy()
+	ps['percent'] = True
+	ps['bins'] = 5
+	ps['directionalityBins'] = [-1, -0.6, -0.3, -0.1, 0.1, 0.3, 0.6, 1]
+	ps['xStartPosBins'] = 5
+	ps['migrationPersistenceBins'] = np.linspace(0, 1, 6)
+	#ps['norm'] = True
+
+	data.selectData(filters)
+	data.writeData()
+
+	#HOW YOU CAN ACCESS INDIVIDUAL EXPERIMENT FILES
+	#v1 = data.experiments['VEGF0_10']
+	#v1.plotWeightedAverageCorr('xStartPos', 'directionality', ps)
+
+
+	#data.histogramTemporalAnalysis(ps)
+	#data.spatialTemporalAnalysis(ps)
+	data.plotWeightedAverageCorrSummary(ps)
+	data.comparisonAnalysis(ps)
+	#data.cellVisualization('migrationPersistence', ps)
+
+	P.close()
+
+	for experiment, v in data.experiments.items():
+		#v.plotWeightedAverageCorr('age', 'velocity')
+		#v.plotScatter('age', 'velocity')
+		v.plotScatter('xStartPos', 'velocity')
+		#v.plotWeightedAverageCorr('yStartPos', 'velocity')
+
+		#plotBinDataSummary(v, 'directionality', ps)
+		#v.plotWeightedAverageCorr('xStartPos', 'directionality', ps)
+
+		#plotBinDataSummary(v, 'xStartPos', ps);
+		#v.histogramScan('directionality', 'xStartPos', ps)
+		#v.histogramScan('directionality', 'concentration', ps)
+
+		P.close()
+
+
+
+
--- lib/TrackClassGlobals.py
+++ lib/TrackClassGlobals.py
@@ -12,23 +12,22 @@ FIELD_VECTOR_INSTANCE = [-1, 0]
 
 DEFAULT_TRACK_FIELDS = { 
 
-	'absVelocity': getAbsVelocity,
-	'age': getAge,
-	'avgMov': getAverageMovement,
-	'concentration': getConcentration,
-	'directionality': getDirectionality, 
-	'migrationPersistence': getMigrationPersistence,
-	'velocity': getVelocity,
-	'xMigrationSpeed': getxMigrationSpeed,
-	'yMigrationSpeed': getyMigrationSpeed,
-
-	'xStartPos': getxStartPos,
-	'xEndPos': getxEndPos,
-	'yStartPos': getyStartPos,
-	'yEndPos': getyEndPos,
-	'numFrames': getNumFrames,
-	'firstFrame': getFirstFrame,
-	'lastFrame': getLastFrame
+	'absVelocity'          : getAbsVelocity,
+	'age'                  : getAge,
+	'avgMov'               : getAverageMovement,
+	'concentration'        : getConcentration,
+	'directionality'       : getDirectionality,
+	'migrationPersistence' : getMigrationPersistence,
+	'velocity'             : getVelocity,
+	'xMigrationSpeed'      : getxMigrationSpeed,
+	'yMigrationSpeed'      : getyMigrationSpeed,
+	'xStartPos'            : getxStartPos,
+	'xEndPos'              : getxEndPos,
+	'yStartPos'            : getyStartPos,
+	'yEndPos'              : getyEndPos,
+	'numFrames'            : getNumFrames,
+	'firstFrame'           : getFirstFrame,
+	'lastFrame'            : getLastFrame
 
 }
 
@@ -41,11 +40,11 @@ DEAULT_TRACK_FILE_FIELDS = {
 
 Default_Exp_Params = {
 	
-	'gradient': 0,
-	'reverse': False,
-	'maxX': 0,
-	'maxY': 0,
-	'spatialConversionFactor': 0.64
+	'gradient'                : 0,
+	'reverse'                 : False,
+	'maxX'                    : 0,
+	'maxY'                    : 0,
+	'spatialConversionFactor' : 0.64
 
 
 }
@@ -53,52 +52,51 @@ Default_Exp_Params = {
 
 axesLabels = {
 
-	'absVelocity': 'absVelocity: microns/hour',
-	'age': 'age: frames',
-	'avgMov': 'avgMov: microns/hour',
-	'concentration': 'concentration: ug',
-	'directionality': 'directionality', 
-	'migrationPersistence': 'migration persistence',
-	'velocity': 'velocity: microns/hour',
-	'xMigrationSpeed': 'xMigrationSpeed: microns/hour',
-	'yMigrationSpeed': 'yMigrationSpeed: microns/hour',
-
-	'xStartPos': 'xStartPos: microns',
-	'xEndPos': 'xEndPos: microns',
-	'yStartPos': 'yStartPos: microns',
-	'yEndPos': 'yEndPos: microns',
-	'numFrames': 'numFrames: frames',
-	'firstFrame': 'firstFrame: frame',
-	'lastFrame': 'lastFrame: frame'
+	'absVelocity'          : 'absVelocity: microns/hour',
+	'age'                  : 'age: frames',
+	'avgMov'               : 'avgMov: microns/hour',
+	'concentration'        : 'concentration: ug',
+	'directionality'       : 'directionality',
+	'migrationPersistence' : 'migration persistence',
+	'velocity'             : 'velocity: microns/hour',
+	'xMigrationSpeed'      : 'xMigrationSpeed: microns/hour',
+	'yMigrationSpeed'      : 'yMigrationSpeed: microns/hour',
+	'xStartPos'            : 'xStartPos: microns',
+	'xEndPos'              : 'xEndPos: microns',
+	'yStartPos'            : 'yStartPos: microns',
+	'yEndPos'              : 'yEndPos: microns',
+	'numFrames'            : 'numFrames: frames',
+	'firstFrame'           : 'firstFrame: frame',
+	'lastFrame'            : 'lastFrame: frame'
 
 }
 
 
 axesLimits = {
 	
-	'xStartPos': (0, 10000),
-	'yStartPos': (0, 10000),
-	'directionality': (-1, 1),
-	'avgMov': (0, 10),
-	'age': (0, 100),
-	'migrationPersistence': (0, 1),
-	'velocity': (0, 20)
+	'xStartPos'            : (0, 10000),
+	'yStartPos'            : (0, 10000),
+	'directionality'       : (-1, 1),
+	'avgMov'               : (0, 10),
+	'age'                  : (0, 100),
+	'migrationPersistence' : (0, 1),
+	'velocity'             : (0, 20)
 
 }
 
 
 ColorMapPropertyDict = {
 	#diverging colormap
-	'directionality': 'jet',
-	'migrationPersistence': 'jet',
+	'directionality'       : 'jet',
+	'migrationPersistence' : 'jet',
 
 	#sequential colormap
-	'velocity': 'YlOrRd',
-	'avgMov': 'YlOrRd',
-	'xStartPos': 'YlOrRd',
-	'yStartPos': 'YlOrRd',
-	'startFrame': 'YlOrRd',
-	'endFrame': 'YlOrRd'
+	'velocity'             : 'YlOrRd',
+	'avgMov'               : 'YlOrRd',
+	'xStartPos'            : 'YlOrRd',
+	'yStartPos'            : 'YlOrRd',
+	'startFrame'           : 'YlOrRd',
+	'endFrame'             : 'YlOrRd'
 }
 
 
@@ -106,89 +104,89 @@ maxRange = [[float('-inf'), float('inf')]]
 
 DefaultFilters = {
 	
-		'frames': maxRange,
-		'xPos': maxRange,
-		'yPos': maxRange,
+	'frames'               : maxRange,
+	'xPos'                 : maxRange,
+	'yPos'                 : maxRange,
 
-		#enter scalar field values. multiple ranges possible
-		'absVelocity': maxRange,
-		'age': maxRange,
-		'avgMov': maxRange,
-		'concentration': maxRange,
-		'directionality': maxRange, 
-		'migrationPersistence': maxRange,
-		'velocity': maxRange,
-		'xMigrationSpeed': maxRange,
-		'yMigrationSpeed': maxRange,
+	#enter scalar field values. multiple ranges possible
+	'absVelocity'          : maxRange,
+	'age'                  : maxRange,
+	'avgMov'               : maxRange,
+	'concentration'        : maxRange,
+	'directionality'       : maxRange,
+	'migrationPersistence' : maxRange,
+	'velocity'             : maxRange,
+	'xMigrationSpeed'      : maxRange,
+	'yMigrationSpeed'      : maxRange,
 
-		'xStartPos': maxRange,
-		'xEndPos': maxRange,
-		'yStartPos': maxRange,
-		'yEndPos': maxRange,
-		'numFrames': maxRange,
-		'firstFrame': maxRange,
-		'lastFrame': maxRange,
+	'xStartPos'            : maxRange,
+	'xEndPos'              : maxRange,
+	'yStartPos'            : maxRange,
+	'yEndPos'              : maxRange,
+	'numFrames'            : maxRange,
+	'firstFrame'           : maxRange,
+	'lastFrame'            : maxRange,
 
 		}
 
 #plot defualts
 PlotDefaults = {
 	
-		#general plot settings
-		'show': False,
-		'save': True,
-		'bins': 20,
-		'norm': False,
-		'percent': False,
-		'title': '',
-		'weight': 'age',
-		'legend': True,
-		'legendLoc': 1,
-		'newFig': True,
-		'stdErrorBars': True,
+	#general plot settings
+	'show'                     : False,
+	'save'                     : True,
+	'bins'                     : 20,
+	'norm'                     : False,
+	'percent'                  : False,
+	'title'                    : '',
+	'weight'                   : 'age',
+	'legend'                   : True,
+	'legendLoc'                : 1,
+	'newFig'                   : True,
+	'stdErrorBars'             : True,
 
-		#movie settings
-		'movie': True,
+	#movie settings
+	'movie'                    : True,
 
-		#temporal analysis settings
-		'startFrame': 0,
-		'endFrame': 30,
-		'frameInterval': 10, #TODO: MAKE THIS FRAMBINS. RIGHT NOW THIS SPECIFIES EXACT FRAMES IN EACH BIN
-		'startConcentration': 0,
-		'endConcentration': 10,
-		'concentrationInterval': 2,
-		'xStartPosBins': 20,
-		'directionalityBins': 10,
-		'migrationPersistenceBins': 10,
-		'velocityBins': 10,
-		'avgMovBins': 10,
-		'concentrationBins': 10,
-
-
-		#spatial analysis settings
-		'spatialBins': 10,
-
-		#rotation angles for 3dPlotMovie
-		'rotStartAngle': 220,
-		'rotEndAngle': 221,
-		'rotResolution': 1,
+	#temporal analysis settings
+	'startFrame'               : 0,
+	'endFrame'                 : 30,
+	'frameInterval'            : 10, #TODO: MAKE THIS FRAMBINS. RIGHT NOW THIS SPECIFIES EXACT FRAMES IN EACH BIN
+	'startConcentration'       : 0,
+	'endConcentration'         : 10,
+	'concentrationInterval'    : 2,
+	'xStartPosBins'            : 20,
+	'directionalityBins'       : 10,
+	'migrationPersistenceBins' : 10,
+	'velocityBins'             : 10,
+	'avgMovBins'               : 10,
+	'concentrationBins'        : 10,
 
 
-		#cellVisualization settings
-		'colorMin': -1,
-		'colorMax': 1,
+	#spatial analysis settings
+	'spatialBins'              : 10,
 
-		#percents
-		'percents': [0, 25, 50, 75, 100]
+	#rotation angles for 3dPlotMovie
+	'rotStartAngle'            : 220,
+	'rotEndAngle'              : 221,
+	'rotResolution'            : 1,
+
+
+	#cellVisualization settings
+	'colorMin'                 : -1,
+	'colorMax'                 : 1,
+
+	#percents
+	'percents'                 : [0, 25, 50, 75, 100]
 
 }
 
 AnimationDefaults = {
 		
-		#temporal analysis settings
-		'startFrame': 0,
-		'endFrame': 0,
-		'frameInterval': 0, #TODO: MAKE THIS FRAMBINS. RIGHT NOW THIS SPECIFIES EXACT FRAMES IN EACH BIN
+	#temporal analysis settings
+	'startFrame'    : 0,
+	'endFrame'      : 0,
+	'frameInterval' : 0, #TODO: MAKE THIS FRAMBINS. RIGHT NOW THIS SPECIFIES EXACT FRAMES IN EACH BIN
 
 
 
